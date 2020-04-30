ac49e588e3311f998201ad82dfeaf4caa578550d
Abdulmajeed Alyafey <a.ahmed@morniksa.com>
2020-04-30 23:44:31 +0300

    Writing first Espresso test

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -2,19 +2,20 @@ package com.example.android.architecture.blueprints.todoapp.taskdetail
 
 import FakeAndroidTestRepository
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.test.filters.MediumTest
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
 import com.example.android.architecture.blueprints.todoapp.R
 import com.example.android.architecture.blueprints.todoapp.ServiceLocator
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.After
-import org.junit.Before
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 
 /**
  * Created by Abdulmajeed Alyafey on 4/30/20.
@@ -41,12 +42,28 @@ class TaskDetailFragmentTest {
     @Test
     fun activeTaskDetails_DisplayedInUI() = runBlockingTest {
         // GIVEN - Add active (incomplete) task to the DB
-        val activeTask = Task("Active Task", "AndroidX Rocks", true)
+        val activeTask = Task("Active Task", "AndroidX Rocks", false)
         repository.saveTask(activeTask)
 
         // WHEN - Details fragment launched to display task
         val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
         launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
-        Thread.sleep(2000)
+
+        /** (Espresso Usage) **/
+        // THEN - Task details are displayed on the screen
+        onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("Active Task")))
+
+        onView(withId(R.id.task_detail_description_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText("AndroidX Rocks")))
+
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isChecked()))
+
+        // You can perform action on the view, example check the radio button.
+        //onView(withId(R.id.task_detail_complete_checkbox)).perform(click())
+
+        Thread.sleep(2000) // Optional just to slow down test and take a look at the UI.
     }
+    
 }
